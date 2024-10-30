8ec65d4386018ae7945c7a222bb7f566cd18bb08
khue <luumsk@gmail.com>
2024-10-30 18:31:48 +0700

    fix error of nnUNet prediction, not the server is running, but does not clean up temp files

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 import tempfile
 import subprocess
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks
 from fastapi.responses import FileResponse, JSONResponse
 
 # Set nnUNet environment variables
 os.environ["nnUNet_raw"] = "data/nnUNet_raw"
 os.environ["nnUNet_preprocessed"] = "data/nnUNet_preprocessed"
 os.environ["nnUNet_results"] = "data/nnUNet_results"
+
+print("nnUNet_raw:", os.environ.get("nnUNet_raw"))
+print("nnUNet_preprocessed:", os.environ.get("nnUNet_preprocessed"))
+print("nnUNet_results:", os.environ.get("nnUNet_results"))
 
 app = FastAPI()
 
@@ -25,8 +29,12 @@ async def root():
         }
     )
 
+def cleanup_temp_files(paths):
+    for path in paths:
+        os.remove(path)
+
 @app.post("/segment/")
-async def segment(files: list[UploadFile] = File(...)):
+async def segment(files: list[UploadFile] = File(...), background_tasks: BackgroundTasks = BackgroundTasks()):
     # Check that exactly four files are uploaded
     if len(files) != 4:
         return JSONResponse(
@@ -45,35 +53,38 @@ async def segment(files: list[UploadFile] = File(...)):
                 temp_file.write(await file.read())
             temp_input_paths.append(file_path)
 
-        # Create a temporary directory for the output
-        with tempfile.TemporaryDirectory() as temp_output_dir:
-            command = [
-                "nnUNetv2_predict",
-                "-i", temp_input_dir,
-                "-o", temp_output_dir,
-                "-d", "111",  # dataset id
-                "-f", "0",  # fold 0
-                "-c", "3d_fullres",  # configuration
-                "-tr", "nnUNetTrainer_TverskyBCE",  # trainer
-                "-chk", "checkpoint_final.pth"
-            ]
-            print(command)
+        output_dir = os.path.join(os.environ.get("nnUNet_results"), "Dataset111_Meta", "nnUNetTrainer_TverskyBCE__nnUNetPlans__3d_fullres")
+        command = [
+            "nnUNetv2_predict",
+            "-i", temp_input_dir,
+            "-o", output_dir,
+            "-d", "111",  # dataset id
+            "-f", "0",  # fold 0
+            "-c", "3d_fullres",  # configuration
+            "-tr", "nnUNetTrainer_TverskyBCE",  # trainer
+            "-chk", "checkpoint_final.pth"
+        ]
+        print(command)
 
-            # Execute the command
-            try:
-                subprocess.run(command, check=True)
-            except subprocess.CalledProcessError as e:
-                return JSONResponse(content={"error": f"Prediction failed: {e}"}, status_code=500)
+        # Execute the command
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            return JSONResponse(content={"error": f"Prediction failed: {e}"}, status_code=500)
 
-            # Locate the output segmentation file in the temporary output directory
-            output_file_path = os.path.join(temp_output_dir, os.listdir(temp_output_dir)[0])
+        # Locate the output segmentation file in the output directory
+        output_file_path = os.path.join(output_dir, os.listdir(output_dir)[0])
+        print('output file path:', output_file_path)
 
-            # Return the segmented NIfTI file as the response
-            response = FileResponse(output_file_path, media_type="application/gzip", filename="segmentation_output.nii.gz")
+        # Return the segmented NIfTI file as the response
+        response = FileResponse(output_file_path, media_type="application/gzip", filename="segmentation_output.nii.gz")
 
-            # Clean up input files after response is sent
-            response.call_on_close(lambda: [os.remove(path) for path in temp_input_paths])
-            return response
+        # # Add cleanup task to delete temporary input files after response
+        # background_tasks.add_task(cleanup_temp_files, temp_input_paths)
+        # background_tasks.add_task(cleanup_temp_files, [output_file_path])
+
+        return response
+
 
 if __name__ == "__main__":
     import uvicorn
