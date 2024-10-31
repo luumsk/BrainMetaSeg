30086bae78b536ea90a7c95fbd1036c8d108cd12
khue <luumsk@gmail.com>
2024-10-31 11:24:44 +0700

    do not use temp folders

--- app.py
+++ app.py
@@ -33,55 +33,51 @@ def cleanup_temp_files(paths):
 
 @app.post("/segment/")
 async def segment(files: list[UploadFile] = File(...), background_tasks: BackgroundTasks = BackgroundTasks()):
-    # Check that exactly four files are uploaded
     if len(files) != 4:
         return JSONResponse(
             content={"error": "Exactly four files are required, in the order: T1C, T1N, T2F, T2W."},
             status_code=400
         )
 
-    # Create a temporary directory for input files
-    with tempfile.TemporaryDirectory() as temp_input_dir:
-        temp_input_paths = []
-
-        # Save each uploaded file in the temporary directory
-        for file in files:
+    temp_input_dir = os.environ.get("nnUNet_raw")
+    temp_input_paths = []
+    for file in files:
             file_path = os.path.join(temp_input_dir, file.filename)
             with open(file_path, "wb") as temp_file:
                 temp_file.write(await file.read())
             temp_input_paths.append(file_path)
 
-        temp_output_dir = os.environ.get("nnUNet_output")
-        command = [
-            "nnUNetv2_predict",
-            "-i", temp_input_dir,
-            "-o", temp_output_dir,
-            "-d", "111",  # dataset id
-            "-f", "0",  # fold 0
-            "-c", "3d_fullres",  # configuration
-            "-tr", "nnUNetTrainer_TverskyBCE",  # trainer
-            "-chk", "checkpoint_final.pth",
-            "--disable_progress_bar"
-        ]
+    temp_output_dir = os.environ.get("nnUNet_output")
+    command = [
+        "nnUNetv2_predict",
+        "-i", temp_input_dir,
+        "-o", temp_output_dir,
+        "-d", "111",  # dataset id
+        "-f", "0",  # fold 0
+        "-c", "3d_fullres",  # configuration
+        "-tr", "nnUNetTrainer_TverskyBCE",  # trainer
+        "-chk", "checkpoint_final.pth",
+        "--disable_progress_bar"
+    ]
 
-        # Execute the command
-        try:
-            subprocess.run(command, check=True)
-        except subprocess.CalledProcessError as e:
-            return JSONResponse(content={"error": f"Prediction failed: {e}"}, status_code=500)
+    # Execute the command
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        return JSONResponse(content={"error": f"Prediction failed: {e}"}, status_code=500)
 
-        # Locate the output segmentation file in the output directory
-        temp_output_paths = [os.path.join(temp_output_dir, fn) for fn in os.listdir(temp_output_dir)]
-        output_file_path = [pth for pth in temp_output_paths if pth.endswith('.nii.gz')][0]
+    # Locate the output segmentation file in the output directory
+    temp_output_paths = [os.path.join(temp_output_dir, fn) for fn in os.listdir(temp_output_dir)]
+    output_file_path = [pth for pth in temp_output_paths if pth.endswith('.nii.gz')][0]
 
-        # Return the segmented NIfTI file as the response
-        response = FileResponse(output_file_path, media_type="application/gzip", filename="segmentation_output.nii.gz")
+    # Return the segmented NIfTI file as the response
+    response = FileResponse(output_file_path, media_type="application/gzip", filename="segmentation_output.nii.gz")
 
-        # Schedule cleanup for only the temporary input files
-        background_tasks.add_task(cleanup_temp_files, temp_input_paths)
-        background_tasks.add_task(cleanup_temp_files, temp_output_paths)
+    # Schedule cleanup for only the temporary input files
+    background_tasks.add_task(cleanup_temp_files, temp_input_paths)
+    background_tasks.add_task(cleanup_temp_files, temp_output_paths)
 
-        return response
+    return response
 
 
 if __name__ == "__main__":
